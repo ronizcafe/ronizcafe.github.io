de001f83b7757d5dfb0192b06709a9417c019fc2
Mihreteab <mihreteabdemeke@gmail.com>
2025-06-02 16:27:57 +0300

    chore: add prefix on config

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,13 @@ const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export', // Enable static export
-  assetPrefix: isProd ? '/your-repo-name/' : '', // For GitHub Pages subpath
+  output: 'export', // This replaces next export
+  assetPrefix: isProd ? '/ronizcafe.github.io/' : '', // For GitHub Pages subpath
   images: {
     unoptimized: true, // Required for static export
   },
+  // Optional: Add basePath if deploying to project site (not user site)
+  basePath: isProd ? '/ronizcafe.github.io' : '',
 };
 
 export default nextConfig;
