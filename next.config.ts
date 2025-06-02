7d42f6f43cc69e58399afbb7f72465e6616d2ca8
Mihreteab <mihreteabdemeke@gmail.com>
2025-06-02 16:19:00 +0300

    chore: add github action

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export', // Enable static export
+  assetPrefix: isProd ? '/your-repo-name/' : '', // For GitHub Pages subpath
+  images: {
+    unoptimized: true, // Required for static export
+  },
 };
 
 export default nextConfig;
